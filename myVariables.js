305ba61b6dfc7fce1bcc7a66a08ea99b4a1ddd70
Eyad911 <Eyad.Abdulhamid.elq.js@tuwaiq.edu.sa>
2021-10-14 11:53:14 +0300

    T1

---
+++ myVariables.js
@@ -0,0 +1,24 @@
+
+
+let language = 'JavaScript ';
+let score = 10;
+
+let length = 20;
+let width = 25;
+
+let num1 =22;
+let num2 =10;
+
+console.log(language + score );
+
+
+let rem_area = length * width;
+
+console.log(`The area of the rectangle is ${rem_area}`);
+
+let Div = Math.trunc(num1/num2);
+
+console.log(`${num1} + ${num2}= ${Div}  remainder ${num1%num2}`);
+
+
+

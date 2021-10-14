25998de9004a94720d1d6cd6ada34b8969fcb921
Eyad911 <Eyad.Abdulhamid.elq.js@tuwaiq.edu.sa>
2021-10-14 12:16:32 +0300

    T2

---
+++ dataTypes.js
@@ -0,0 +1,16 @@
+let x = 10;
+let y = "hello world";
+let z = [5, 7, 8, "A", 2, "B"]; //DataType -> object
+let A; // undefind
+let B = null;
+
+let Student = {
+  Name: "Eyad",
+  ID: "555",
+  course: "JS",
+};
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
+console.log(Student, typeof Student);
--- myVariables.js
+++ myVariables.js
@@ -1,24 +1,18 @@
-
-
-let language = 'JavaScript ';
+let language = "JavaScript ";
 let score = 10;
 
 let length = 20;
 let width = 25;
 
-let num1 =22;
-let num2 =10;
+let num1 = 22;
+let num2 = 10;
 
-console.log(language + score );
-
+console.log(language + score);
 
 let rem_area = length * width;
 
 console.log(`The area of the rectangle is ${rem_area}`);
 
-let Div = Math.trunc(num1/num2);
-let mod = num1%num2;
+let Div = Math.trunc(num1 / num2);
+let mod = num1 % num2;
 console.log(`${num1} + ${num2}= ${Div}  remainder ${mod}`);
-
-
-
